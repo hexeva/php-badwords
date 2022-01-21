d31a36775c08fde3d3df00a93986bf351a583ec1
hexeva <patrick79.g@gmail.com>
2022-01-21 16:01:04 +0100

    created variable with get

--- index.php
+++ index.php
@@ -8,7 +8,34 @@
 </head>
 <body>
 
-<p>TEST</p>
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
+
+<?php 
+//CREO VARIABILE 
+    $paragraph= 'Lo spreco della vita si trova nell\'amore che non si è saputo dare, nel potere che non si è saputo utilizzare, nell\'egoistica prudenza che ci ha impedito di rischiare e che, evitandoci un dispiacere, ci ha fatto mancare la felicità.';
+?>
+
+<h3>CREO VARIABILE E STAMPO A SCHERMO</h3>
+
+<h2> <?php echo $paragraph; ?> </h2>
+
+
+<h3>STAMPO LA LUNGHEZZA DELLA VARIABILE</h3>
+
+<?php 
+    $paragraph_length = strlen($paragraph);
+?>
+
+<h2> LUNGHEZZA PARAGRAFO: <?php echo $paragraph_length; ?> </h2>
+
+
+
+
     
+
 </body>
 </html>
