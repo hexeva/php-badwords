729a264a2caf66c2bbf1b2a573adb1b268598953
hexeva <patrick79.g@gmail.com>
2022-01-24 16:10:11 +0100

    push

--- index.php
+++ index.php
@@ -35,25 +35,29 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 <!-- CREO VARIABILE DA PASSARE CON GET -->
 
 <?php 
-    $not_censored = $_GET['word'];
+    $to_censored = $_GET['word'];
+    // creo una stringa a cui passo la variabile da get
+
+    $string = " L'esperienza è il tipo di insegnante più difficile. Prima ti fa l'esame, poi ti spiega la" . ' ' . $to_censored;
+    $string_length = strlen($string);
+
 ?>
 
-<!-- passo la la variablie nel paragrafo -->
+<!-- stampo a schermo stringa e lunghezza-->
+
+<h2> <?php echo $string; ?> </h2>
+<h3> <?php echo $string_length ?> </h3>
+
+<!-- Rimpiazzo la variabile da censurare  utilizzando str replace  -->
 
 <?php 
-    $paragraph = 'Lo spreco della vita si trova nell\'amore che non si è saputo dare, nel potere che non si è saputo utilizzare, nell\'egoistica prudenza che ci ha impedito di rischiare e che, evitandoci un dispiacere, ci ha fatto mancare la'. '  '. $not_censored;
+    $second_string = str_replace($to_censored,'***',$string);
+    // var_dump($second_string);
+    $final_length = strlen($second_string);
 ?>
-
-<h2> <?php echo $paragraph; ?> </h2>
-
-<?php
-    $paragraph_length = strlen($paragraph);
-    // var_dump($paragraph_length);
-?>
-
-<h2>LUNGHEZZA PARAGRAFO: <?php echo $paragraph_length; ?></h2>
-
-
+<!-- stampo a schermo stringa rimpiazzata e sua lunghezza -->
+<h2> <?php echo $second_string; ?> </h2>
+<h3> <?php echo $final_length ?> </h3>
 
 </body>
 </html>
